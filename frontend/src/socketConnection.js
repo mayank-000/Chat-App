172c311ac23a38042da0e5f38156f57e9c7762e2
Mayank Sagar <mayanksagar159@gmail.com>
2026-01-13 18:53:35 +0530

    Refactor BACKEND_URL initialization in socketConnection
    Removed fallback URL for BACKEND_URL and ensured it always replaces '/api'.

--- frontend/src/socketConnection.js
+++ frontend/src/socketConnection.js
@@ -1,9 +1,7 @@
 import { io } from "socket.io-client";
 
-const BACKEND_URL = process.env.REACT_APP_API_URL 
-    ? process.env.REACT_APP_API_URL.replace('/api', '') 
-    : 'http://localhost:6060';
-
+const BACKEND_URL = process.env.REACT_APP_API_URL.replace('/api', '');
+    
 const socket = io(BACKEND_URL, {
     autoConnect: false, // Changed from false to true
     withCredentials: true,
